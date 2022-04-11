4f9225c6f158f6b215851cf8d134bb5cabaf67da
yannis bleiel <yannisbleiel@yanniss-MacBook-Pro.local>
2022-04-11 13:44:19 +0200

    memory leak

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ybleiel <ybleiel@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 13:20:02 by ybleiel           #+#    #+#             */
-/*   Updated: 2022/04/08 14:37:43 by ybleiel          ###   ########.fr       */
+/*   Updated: 2022/04/11 13:42:57 by ybleiel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,26 +43,32 @@ void	malloc_ps(t_pushswap *ps, char **argv)
 	int	d;
 
 	d = stack_size(argv);
-	// printf("%d\n", sizeof(d));
-	ps->stack_a = malloc(sizeof(char **) * d);
-	ps->stack_b = malloc(sizeof(char **) * d);
-	ps->index = malloc(sizeof(int *) * d);
+	ps->stack_a = malloc(sizeof(char *) * d);
+	ps->stack_b = malloc(sizeof(char *) * d);
+	ps->index = malloc(sizeof(int) * d);
 }
 
 void	free_ps(t_pushswap *ps)
 {
+	int i;
+
+	i = 0;
+	while (ps->stack_a[i])
+	{
+		free(ps->stack_a[i]);
+		i++;
+	}
 	free(ps);
 	free(ps->stack_a);
 	free(ps->stack_b);
-	free(index);
-	exit(0);
+	free(ps->index);
 }
 
 void	main_sort(t_pushswap *ps, int j)
 {
 	check_duplicate(ps);
 	if (check_order(ps) == 0)
-		exit(0);
+		free_ps(ps);
 	index_sort(ps);
 	if (j == 3)
 		two_sort(ps);
@@ -79,14 +85,11 @@ int	main(int argc, char **argv)
 	t_pushswap	*ps;
 	int			j;
 	int			n;
-	char		*str;
-	int			i;
 
-	i = 0;
 	j = 1;
 	n = 0;
-	if (argc <= 1)
-		exit(0);
+	if (argc <= 2)
+		return (0);
 	ps = malloc(sizeof(t_pushswap));
 	malloc_ps(ps, argv);
 	while (argv[j])
@@ -94,12 +97,11 @@ int	main(int argc, char **argv)
 		check_digit(argv[j]);
 		ps->stack_a[n] = malloc(ft_strlen(argv[j]));
 		ps->stack_a[n] = argv[j];
-		str = argv[j];
-		i = str[i] + '0';
 		n++;
 		j++;
 	}
 	main_sort(ps, j);
-	exit(0);
 	free_ps(ps);
+	return (0);
+	exit(0);
 }
